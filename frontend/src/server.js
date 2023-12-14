853fa41e0cd81162c98f0539b874edcac99692cc
ImamNurhadi <adigaming015@mail.com>
2023-12-14 15:37:29 +0700

    second commit

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -7,23 +7,14 @@ app.use(express.json());
 // Simulasi penyimpanan data pendaftaran
 const registrations = [];
 
-// Middleware untuk validasi teks
-const validateTextMiddleware = (req, res, next) => {
-  const { text } = req.body;
-  // Contoh validasi sederhana
-  if (text.toLowerCase().includes('kata buruk')) {
-    return res.status(403).send('Teks mengandung kata-kata buruk.');
-  }
-  next();
-};
 
 // Endpoint untuk mendapatkan data pendaftaran
-app.get('/api/pendaftaran', (req, res) => {
+app.get('http://localhost:3000/api/pendaftaran', (req, res) => {
   res.json({ docs: registrations });
 });
 
 // Endpoint untuk menambahkan pendaftaran baru
-app.post('/api/pendaftaran', validateTextMiddleware, (req, res) => {
+app.post('http://localhost:3000/api/pendaftaran', validateTextMiddleware, (req, res) => {
   const newRegistration = req.body;
   registrations.push(newRegistration);
   res.status(201).json(newRegistration);
